6bf6a0ba97c543bf96ba083750e03038590bfc15
Yousuf Ali <yousuf.pharma@gmail.com>
2023-10-07 01:09:22 -0400

    10-07-2023

--- get_todays_photo.sh
+++ get_todays_photo.sh
@@ -4,7 +4,7 @@ mkdir $t_date
 touch $t_date.md
 # echo "## $t_date" >> README.md
 # echo "" >> README.md
-osxphotos export $t_date --album Activity_zaarif --convert-to-jpeg --added-in-last '1d' --skip-original-if-edited
+osxphotos export $t_date --album Activity_zaarif --convert-to-jpeg --jpeg-quality .4 --added-in-last '2d' --skip-original-if-edited
 readarray -t array < <(ls $t_date)
 
 for i in ${array[@]};do
