cf26e9d9948b956ebf90d15f73432a10fe53bedd
Yousuf Ali <yousuf.pharma@gmail.com>
2023-10-13 21:52:01 -0400

    10-13-2023

--- get_todays_photo.sh
+++ get_todays_photo.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
-# t_date=$(date +'%m-%d-%Y')
-t_date=10-12-2023
+t_date=$(date +'%m-%d-%Y')
+# t_date=10-12-2023
 mkdir $t_date
 touch $t_date.md
 # echo "## $t_date" >> README.md
