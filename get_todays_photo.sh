80292978de4d333c0b737bfb2a343369d08d2e41
Yousuf Ali <yousuf.pharma@gmail.com>
2023-12-04 22:27:10 -0500

    11-19-2023

--- get_todays_photo.sh
+++ get_todays_photo.sh
@@ -1,11 +1,13 @@
 #!/usr/local/bin/bash
 # t_date=$(date +'%m-%d-%Y')
-t_date=11-18-2023
+t_date=11-19-2023
 mkdir -p $t_date
 touch $t_date.md
 # echo "## $t_date" >> README.md
 # echo "" >> README.md
-osxphotos export $t_date --album Activity_zaarif --convert-to-jpeg --jpeg-quality .4 --added-in-last '1d' --skip-original-if-edited
+# osxphotos export $t_date --album Activity_zaarif --convert-to-jpeg --jpeg-quality .4 --added-in-last '1d' --skip-original-if-edited
+
+osxphotos export $t_date --album Activity_zaarif --convert-to-jpeg --jpeg-quality .4  --skip-original-if-edited
 readarray -t array < <(ls $t_date)
 
 for i in ${array[@]};do
